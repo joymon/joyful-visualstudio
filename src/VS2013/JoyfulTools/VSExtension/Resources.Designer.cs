18dbc2361f5fa9abe35ee3e8bc9fb707b0785fbc
joymon <joymon@gmail.com>
2015-10-12 15:01:58 -0400

    Changes to Extension metadata

--- src/VS2013/JoyfulTools/VSExtension/Resources.Designer.cs
+++ src/VS2013/JoyfulTools/VSExtension/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Joymon.MultipleBlankLinesToSingle {
+namespace JoyfulTools.VSExtension {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Joymon.MultipleBlankLinesToSingle {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Joymon.MultipleBlankLinesToSingle.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("JoyfulTools.VSExtension.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
