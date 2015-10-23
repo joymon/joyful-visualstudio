1df1838d4c0cc964e28a2755637739fda579876e
joymon <joymon@gmail.com>
2015-10-23 17:10:28 -0400

    Added missing file StringExtensions

---
+++ src/VS2013/JoyfulTools/VSExtension/CommonUtilities/StringExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace JoyfulTools.VSExtension
+{
+    public static class StringExtensions
+    {
+        public static bool IsValidXml(this string xml)
+        {
+            try
+            {
+                XDocument.Parse(xml);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
