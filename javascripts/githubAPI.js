8676000a4bd5f481d26d2886df6ece15e3036c03
Joy George Kunjikkuru <joymon@gmail.com>
2015-10-26 22:31:11 -0400

    Create githubAPI.js

---
+++ javascripts/githubAPI.js
@@ -0,0 +1,31 @@
+    
+    loadProjectDescription("https://api.github.com/repos/joymon/joyful-visualstudio")
+    loadReadMe(getGitHubAPIURL());
+    function loadReadMe(url) {
+        var contentContainerId = "#main_content";
+        $.ajax({
+            url: url,
+            dataType: 'jsonp',
+            success: function (response) {
+                $(contentContainerId).html(GetHTMLFromMarkDown(GetStringFromBase64(response.data.content)));
+            }
+        });
+    }
+    function loadProjectDescription(url) {
+        $.ajax({
+            url: url,
+            dataType: 'jsonp',
+            success: function (response) {
+                $("#desc").html(response.data.description);
+            }
+        });
+    }
+    function getGitHubAPIURL() {
+        return 'https://api.github.com/repos/joymon/joyful-visualstudio/readme';
+    }
+    function GetStringFromBase64(base64) {
+        return atob(base64)
+    }
+    function GetHTMLFromMarkDown(markdownString) {
+        return marked(markdownString)
+    }
