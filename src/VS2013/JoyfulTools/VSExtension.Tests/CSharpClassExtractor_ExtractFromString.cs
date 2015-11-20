4df5d5f27825e530f3abf69ce5cf0c63b3d96d5d
joymon <joymon@gmail.com>
2015-11-20 10:39:20 -0500

    Added CSharpClassExtractor & MoveClassToFile refactoring code snippet

---
+++ src/VS2013/JoyfulTools/VSExtension.Tests/CSharpClassExtractor_ExtractFromString.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using JoyfulTools.VSExtension;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VSExtension.Tests
+{
+    [TestClass]
+    public class CSharpClassExtractor_ExtractFromString
+    {
+        [TestMethod]
+        public void WhenCreateNewCSharpClassExtractor_ReturnNewObject()
+        {
+            CSharpClassExtractor ext = new CSharpClassExtractor();
+            Assert.IsNotNull(ext);
+        }
+        [TestMethod]
+        public void ExtractFromString_ShouldAcceptStringAndReturnArrayOfString()
+        {
+            CSharpClassExtractor ext = new CSharpClassExtractor();
+            string[] classes = ext.ExtractFromString("dummy string");
+            Assert.IsNotNull(classes);
+        }
+        [TestMethod]
+        public void WhenThereIsNoClass_ReturnEmptyArray()
+        {
+            
+        }
+    }
+}
---
+++ src/VS2013/JoyfulTools/VSExtension/CSharpClassExtractor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JoyfulTools.VSExtension
+{
+    internal class CSharpClassExtractor
+    {
+        internal string[] ExtractFromString(string codeFragmentWhichMayContainMultipleClasses)
+        {
+            return new string[1];
+        }
+    }
+}
